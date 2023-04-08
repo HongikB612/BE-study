e607d60cc55560948fc553f7ddd76ccee54cdbe2
yunhwan <yunhwan.choo00@gmail.com>
2023-04-08 18:48:39 +0900

    새로운 할인 정책 개발

---
+++ src/test/java/hello/core2/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,35 @@
+package hello.core2.discount;
+
+import hello.core2.member.Grade;
+import hello.core2.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인")
+    void vip_o(){
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니라면 할인이 적용되지 않음")
+    void vip_x(){
+        //given
+        Member member = new Member(2L, "memberASIC", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+}
